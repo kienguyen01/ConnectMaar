375c7934f435bb66a0268ab6b57bad646dae7575
kienguyen01 <kienguyen01@gmail.com>
2022-04-05 18:52:01 +0700

    solar + houses added

--- Assets/Map.cs
+++ Assets/Map.cs
@@ -6,6 +6,8 @@ public class Map : MonoBehaviour
 {
 
     public GameObject hexPrefab;
+    public GameObject housePrefab;
+    public GameObject solarPrefab;
 
     int WIDTH_MAP = 20;
     int HEIGHT_MAP = 20;
@@ -16,6 +18,7 @@ public class Map : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        House myHouse = housePrefab.GetComponent<House>();
         for (int x = -3; x < WIDTH_MAP - 6; x++)
         {
             for (int y = 9; y < HEIGHT_MAP; y++)
@@ -28,8 +31,23 @@ public class Map : MonoBehaviour
                 }
                 GameObject hex_cell = (GameObject)Instantiate(hexPrefab, new Vector3(xPos, 0, y * zOffset), Quaternion.identity);
                 hex_cell.name = "Hex_" + x + "_" + y;
-
                 hex_cell.transform.SetParent(this.transform);
+
+                if ((x == 3 && y == 12) || (x == 5 && y == 10) || (x == 7 && y == 15) || (x == -2 && y == 19))
+                {
+                    GameObject house_cell = (GameObject)Instantiate(housePrefab, new Vector3(xPos, 0.2f, y * zOffset), Quaternion.identity);
+                    house_cell.transform.SetParent(transform);
+                    house_cell.name = "house_" + x + "_" + y;
+                }
+
+                if((x == -2 && y == 12) || (x == -3 && y == 19) || (x == 5 && y == 19) || (x == -3 && y == 16))
+                {
+                    GameObject solar_cell = (GameObject)Instantiate(solarPrefab, new Vector3(xPos, 0.2f, y * zOffset), Quaternion.identity);
+                    solar_cell.transform.SetParent(transform);
+                    solar_cell.name = "solar_" + x + "_" + y;
+                }
+                
+                
             }
         }
 
