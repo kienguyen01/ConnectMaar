19b3f18a7969da3972646d4919123a184f812655
kienguyen01 <kienguyen01@gmail.com>
2022-04-10 18:48:30 +0700

    tile reformed

--- Assets/Scripts/ConnectorManager.cs
+++ Assets/Scripts/ConnectorManager.cs
@@ -2,17 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public abstract class Connector : MonoBehaviour
+{
+    List<Tile> tiles = new List<Tile>();
+    Tile endingTile;
+    Tile startingTile;
+    int length;
+    Player ownedPlayer;
+}
 public class ConnectorManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    List<Connector> connectors = new List<Connector>();
+
+    private void OnTileChosen()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            chooseTile();
+        }
+    }
+    Tile chooseTile()
+    {
+        Tile tileTouched;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+        RaycastHit hitInfo;
+
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+
+            GameObject tileObjectTouched = hitInfo.collider.transform.gameObject;
+            tileTouched = tileObjectTouched.GetComponent<Tile>();
+
+            MeshRenderer mr = tileTouched.GetComponentInChildren<MeshRenderer>();
+            mr.material.color = Color.red;
+
+            Debug.Log(tileTouched.name);
+            
+            return tileTouched;
+        }
+
+        return null;
     }
 }
