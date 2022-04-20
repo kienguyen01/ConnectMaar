6189c0bd60a08833d597c74d5ea190f0fe964891
kienguyen01 <54843446+kienguyen01@users.noreply.github.com>
2022-04-21 05:50:06 +0700

    Revert "Require solar tutorial node count"

--- Assets/Scripts/Models/Buildings/House.cs
+++ Assets/Scripts/Models/Buildings/House.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class House : Structure
+public class House : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
