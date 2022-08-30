c08034b2c4a57c5fd163470d612484316b9419e2
tsaglisamuel <schaltzchagly@gmail.com>
2022-08-30 23:05:56 +0200

    volume slider and more localized texts added

---
+++ Assets/Scripts/volumeManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class volumeManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+    public void changeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+    private void Save()
+    {
+        //saving the value of slider into music volume keyname
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
